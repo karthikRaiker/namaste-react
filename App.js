5da00a19e23d148c0fbcc39c5dbe5d15339d37f6
karthikraiker <karthikms932@gmail.com>
2023-08-03 13:01:42 +0530

    ep5

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React (Old Way using React.createElement)"
+);
 const heading1 = React.createElement(
   "h2",
   { id: "heading1" },
@@ -8,4 +15,16 @@ const container = React.createElement("div", { id: "conainer" }, [
   heading,
   heading1,
 ]);
-console.log(container);
+
+const HeaderComponent = () => (
+  <div>
+    {container}
+    {console.log(container)}
+    <h1>Namaste ReactJs (Functional component new way using JSX)</h1>
+    <h2>Sub heading</h2>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeaderComponent />);
